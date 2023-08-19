e588ac1019b9fe8378ab234a836903d7e970290e
abbeymaj <abbeymaj@gmail.com>
2023-08-19 16:29:32 -0400

    Reading and splitting pdf

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 import os
 import pickle
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 # Locating dotenv 
@@ -21,11 +23,33 @@ with st.sidebar:
 
 # Creating a the "main" function:
 def main():
+    '''
+    This is the main function which creates a webpage that allows a user to upload a PDF file and chat with it.
+    
+    '''
     # Creating a title for the web page
     st.header("Chat with your PDF File")
-
-
-
+    
+    # Upload a PDF File
+    pdf = st.file_uploader("Please upload your PDF File below.", type='pdf')
+    
+    # Read the PDF file and extract the text from the document
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+        
+        # Creating embeddings
+        
+        
 # Running the application
 if __name__ == '__main__':
     main()
