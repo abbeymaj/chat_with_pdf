a836ac26451a0f44941c3c7ef796efb3b54e59c9
abbeymaj <abbeymaj@gmail.com>
2023-08-21 18:58:21 -0400

    created app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import os
 import pickle
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
 
 
 # Locating dotenv 
@@ -47,8 +52,30 @@ def main():
         )
         chunks = text_splitter.split_text(text=text)
         
-        # Creating embeddings
+        # Defining the vector store name variable
+        vec_store_name = pdf.name[:-4]
         
+        # Checking if the vector store already exists and if yes reading the vector store
+        if os.path.exists(f'{vec_store_name}.pkl'):
+            with open(f'{vec_store_name}.pkl', 'rb') as file_obj:
+                vector_store = pickle.load(file_obj)
+        # Else creating the vector store
+        else:
+            embeddings = OpenAIEmbeddings()
+            vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f'{vec_store_name}.pkl', 'wb') as file_obj:
+                pickle.dump(vector_store, file_obj)
+        
+        # Allow the user to enter questions to query the PDF file
+        question = st.text_input("Ask a question to your PDF File:")
+        
+        if question:
+            documents = vector_store.similarity_search(query=question, k=3)
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type='stuff')
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=documents, question=question)
+            st.write(response)
         
 # Running the application
 if __name__ == '__main__':
